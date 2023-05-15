c711a0473b72d92507083ecc186589c26f5a1744
tsowems <tsowems@gmail.com>
2023-05-15 16:05:36 +0100

    user login

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,47 @@ import {Request, Response} from 'express';
 import {getUserByEmail, createUser} from '../db/users'
 import { random, authentication } from '../helpers';
 
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.send({
+                status: 400,
+                message: "Email/Password not provided"
+            })
+        }
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if (!user) {
+            return res.send({
+                status: 400,
+                message: "Email not found, please register"
+            })
+        }
+        const  expectedPWHash = authentication(user.authentication.salt, password);
+
+        if (user.authentication.password !== expectedPWHash) {
+            return  res.send({
+                status: 403,
+                message: "Wrong password provided"
+            })
+        }
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+        await user.save();
+
+        res.cookie('TAIWO_AUTH', user.authentication.sessionToken, {domain: 'localhost', path: '/'})
+        return res.send({
+                status: 200,
+                message: user
+            }).end();
+       
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
 export const register = async(req: Request, res: Response) => {
     try {
         const {email, password, username} = req.body;
--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,7 +1,8 @@
 import {Router} from 'express';
 
-import { register } from '../controllers/authentication';
+import { login, register } from '../controllers/authentication';
 
 export default (router: Router) => {
     router.post('/auth/register', register);
+    router.post('/auth/login', login);
 }
